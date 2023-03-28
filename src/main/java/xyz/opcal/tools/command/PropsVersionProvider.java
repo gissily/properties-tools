e9ff1354359a19fc39ed3f5d403bb36dddebbd0a
gissily <gissily@outlook.com>
2023-03-28 19:37:33 +0800

    fix version value

--- src/main/java/xyz/opcal/tools/command/PropsVersionProvider.java
+++ src/main/java/xyz/opcal/tools/command/PropsVersionProvider.java
@@ -2,18 +2,25 @@ package xyz.opcal.tools.command;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import picocli.CommandLine.IVersionProvider;
 
 @Component
 public class PropsVersionProvider implements IVersionProvider {
 
+	static final String SUFFIX = "-SNAPSHOT";
+
 	@Value("${info.app.version}")
 	private String version;
 
 	@Override
 	public String[] getVersion() throws Exception {
-		return new String[] { version };
+		String commandVersion = version;
+		if (StringUtils.endsWithIgnoreCase(version, SUFFIX)) {
+			commandVersion = StringUtils.replace(version, SUFFIX, "");
+		}
+		return new String[] { commandVersion };
 	}
 
 }
