e7437ed2ace614a716a4fc03389f5e22c4f07fcc
gissily <gissily@outlook.com>
2023-04-12 18:26:42 +0800

    fix version display

--- ci/script/commit-next-version.sh
+++ ci/script/commit-next-version.sh
@@ -7,19 +7,11 @@ SCRIPT_DIR_PATH=`dirname ${SCRIPT}`
 CI_DIR_PATH=`dirname ${SCRIPT_DIR_PATH}`
 ROOT_PATH=`dirname ${CI_DIR_PATH}`
 
-
 # tag release
-SNAPSHOT_VERSION=$(${ROOT_PATH}/mvnw help:evaluate -Dexpression=project.version | grep "^[^\\[]" |grep -v Download)
-RELEASE_VERSION=$(echo ${SNAPSHOT_VERSION} | cut -d '-' -f 1)
-
-${ROOT_PATH}/mvnw versions:set -DnewVersion=${RELEASE_VERSION}
-${SCRIPT_DIR_PATH}/clean-pom.sh
-
+RELEASE_VERSION=$(${ROOT_PATH}/mvnw help:evaluate -Dexpression=project.version | grep "^[^\\[]" |grep -v Download | cut -d '-' -f 1)
 TAG_NAME=${RELEASE_VERSION}.release
 git tag -a ${TAG_NAME} -m "Release Tag ${RELEASE_VERSION}"
 git push origin ${TAG_NAME}
-
-${ROOT_PATH}/mvnw versions:set -DnewVersion=${SNAPSHOT_VERSION}
 
 # next version
 ${SCRIPT_DIR_PATH}/next-version.sh -${VERSION_DIRECTION:-s}
