e81fd2c4decfe18736d1ab1c35dd8bfc75673c9a
gissily <gissily@outlook.com>
2023-04-18 14:38:46 +0800

    fix release tag

--- ci/script/commit-next-version.sh
+++ ci/script/commit-next-version.sh
@@ -8,12 +8,22 @@ CI_DIR_PATH=`dirname ${SCRIPT_DIR_PATH}`
 ROOT_PATH=`dirname ${CI_DIR_PATH}`
 
 # tag release
-RELEASE_VERSION=$(${ROOT_PATH}/mvnw help:evaluate -Dexpression=project.version | grep "^[^\\[]" |grep -v Download | cut -d '-' -f 1)
+SNAPSHOT_VERSION=$(./mvnw help:evaluate -Dexpression=project.version | grep "^[^\\[]" |grep -v Download)
+RELEASE_VERSION=${SNAPSHOT_VERSION/-SNAPSHOT/}
+
+${ROOT_PATH}/mvnw versions:set -DnewVersion=${RELEASE_VERSION}
+git add . 
+git commit -m "Release ${RELEASE_VERSION}"
+git push
+
+${SCRIPT_DIR_PATH}/clean-pom.sh
+
 TAG_NAME=${RELEASE_VERSION}.release
 git tag -a ${TAG_NAME} -m "Release Tag ${RELEASE_VERSION}"
 git push origin ${TAG_NAME}
 
 # next version
+${ROOT_PATH}/mvnw versions:set -DnewVersion=${SNAPSHOT_VERSION}
 ${SCRIPT_DIR_PATH}/next-version.sh -${VERSION_DIRECTION:-s}
 
 ${SCRIPT_DIR_PATH}/clean-pom.sh
