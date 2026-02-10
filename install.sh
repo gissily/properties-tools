41eac59d9184e1aed737ae993864366ea180e339
gissily <gissily@outlook.com>
2026-02-10 21:33:16 +0800

    update release script

--- install.sh
+++ install.sh
@@ -4,11 +4,33 @@ set -e
 
 OSTYPE=$(uname)
 BIN_SUFFIX=""
+ARCH=""
 
 if [[ "${OSTYPE}" == "Darwin" ]]; then
   BIN_SUFFIX="macos"
-else 
+elif [[ "${OSTYPE}" == "Linux" ]]; then
   BIN_SUFFIX="linux"
+else
+  echo "error: The OS is not supported!"
+  exit 1
+fi
+
+case "$(uname -m)" in
+  'amd64' | 'x86_64')
+    ARCH='x86_64'
+    ;;
+  'arm64' | 'aarch64')
+    ARCH='arm64'
+    ;;
+  *)
+    echo "error: The architecture is not supported!"
+    exit 1
+    ;;
+esac
+
+if [[ "${BIN_SUFFIX}" == "" || ${ARCH} == "" ]]; then
+  echo "error: The OS or architecture is not supported!"
+  exit 1
 fi
 
 RELEASE=$(curl -s https://api.github.com/repos/gissily/properties-tools/releases/latest  | grep tag_name | cut -d '"' -f 4)
@@ -19,8 +41,8 @@ if [[ "${RELEASE}" == "" ]]; then
 fi
 
 RELEASE_KEY="https://github.com/gissily/properties-tools/releases/download/${RELEASE}/Release.asc"
-DOWNLOAD_URL="https://github.com/gissily/properties-tools/releases/download/${RELEASE}/props-${BIN_SUFFIX}"
-DOWNLOAD_URL_ASC="https://github.com/gissily/properties-tools/releases/download/${RELEASE}/props-${BIN_SUFFIX}.asc"
+DOWNLOAD_URL="https://github.com/gissily/properties-tools/releases/download/${RELEASE}/props-${BIN_SUFFIX}-${ARCH}"
+DOWNLOAD_URL_ASC="https://github.com/gissily/properties-tools/releases/download/${RELEASE}/props-${BIN_SUFFIX}-${ARCH}.asc"
 
 echo "downloading ${RELEASE_KEY}"
 curl -L "${RELEASE_KEY}" -o /tmp/Release.asc
